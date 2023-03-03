aac5a176ad8fb06c50424c627b7ba50bb222d91d
Fawaz1077 <107772207+Fawaz1077@users.noreply.github.com>
2023-03-03 11:03:24 -0500

    FINAL

--- src/card-list.js
+++ src/card-list.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 import "fawaz-nice-card/src/fawaz-nice-card.js";
-import "farisflight/src/flight-card.js"
+import "farisflight/src/flight-card.js" 
 
 
 
@@ -23,17 +23,54 @@ class CardList extends LitElement {
 
   render() {
     return html`
-      <fawaz-nice-card></fawaz-nice-card>
-      <fawaz-nice-card></fawaz-nice-card>
-      <fawaz-nice-card></fawaz-nice-card>
-      <fawaz-nice-card></fawaz-nice-card>
-      <fawaz-nice-card></fawaz-nice-card>
+      <fawaz-nice-card>
+         <slot>bro 1</slot>
+      </fawaz-nice-card>
+
+      <flight-card>
+        <slot>sis 1</slot>
+      </flight-card>
+
+      <fawaz-nice-card>
+         <slot>bro 2</slot>
+      </fawaz-nice-card>
+
+      <flight-card>
+        <slot>sis 2</slot>
+      </flight-card>
+
+      <fawaz-nice-card>
+         <slot>bro 3</slot>
+      </fawaz-nice-card>
+
+      <flight-card>
+        <slot>sis 3</slot>
+      </flight-card>
+
+      <fawaz-nice-card>
+         <slot>bro 4</slot>
+      </fawaz-nice-card>
+
+      <flight-card>
+        <slot>sis 4</slot>
+      </flight-card>
+
+      <fawaz-nice-card>
+         <slot>bro 5</slot>
+      </fawaz-nice-card>
       
-      <flight-card></flight-card>
-      <flight-card></flight-card>
-      <flight-card></flight-card>
-      <flight-card></flight-card>
-      <flight-card></flight-card>
+      <flight-card>
+        <slot>sis 5</slot>
+      </flight-card>
+
+     
+
+      
+
+      
+
+     
+
 
 
     `;
